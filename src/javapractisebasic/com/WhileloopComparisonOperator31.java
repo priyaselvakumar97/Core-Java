e2d7a93f98166f33cc4b5b3e68ec9444a166b01d
priyaselvakumar97 <135342736+priyaselvakumar97@users.noreply.github.com>
2023-10-26 12:45:20 +0530

    Loop continuation

---
+++ src/javapractisebasic/com/WhileloopComparisonOperator31.java
@@ -0,0 +1,17 @@
+package javapractisebasic.com;
+
+public class WhileloopComparisonOperator31 {
+
+	public static void main(String[] args) {
+		int a=50;
+		int b=50;
+		
+		      while(a==b) {
+		    	  System.out.println("A is Greater than B");
+		    	  b= b/10;
+		    	 
+		      }
+		      System.out.println(b);
+	}
+
+}
