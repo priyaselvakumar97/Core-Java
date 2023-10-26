ed632088be011c7b96596af571b74fab32f0da38
priyaselvakumar97 <135342736+priyaselvakumar97@users.noreply.github.com>
2023-10-26 12:57:14 +0530

    while loop with no body

---
+++ src/javapractisebasic/com/WhileloopWithNobody31.java
@@ -0,0 +1,14 @@
+package javapractisebasic.com;
+
+public class WhileloopWithNobody31 {
+
+	public static void main(String[] args) {
+		
+		int i=100;
+		int j=200;
+		while(++i <--j);
+		System.out.println("The Midpoint is"+" "+i);
+
+	}
+
+}
