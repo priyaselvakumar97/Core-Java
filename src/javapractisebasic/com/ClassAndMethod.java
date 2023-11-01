41b66f2f14bc7339acc412ec93baf8f0d2ccf26a
priyaselvakumar97 <135342736+priyaselvakumar97@users.noreply.github.com>
2023-11-01 11:52:21 +0530

    Class example

---
+++ src/javapractisebasic/com/ClassAndMethod.java
@@ -0,0 +1,17 @@
+package javapractisebasic.com;
+
+public class ClassAndMethod {
+	public static void main(String[] args) {
+		double width;
+		double height;
+		double depth;
+	}
+		void volume() {
+			System.out.println("Volume is");
+	       System.out.println(width*height*depth);
+		
+		
+		
+	}
+
+}
